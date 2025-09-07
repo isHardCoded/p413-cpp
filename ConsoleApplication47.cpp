ab4cd9cb199c0347cf8ca26b4d28b4a7902677d8
isHardCoded <ishardcoded@gmail.com>
2025-09-07 15:44:23 +0500

    task-1

--- ConsoleApplication47.cpp
+++ ConsoleApplication47.cpp
@@ -1,8 +1,30 @@
 ﻿#include <iostream>
+#define _USE_MATH_DEFINES
+#include <cmath>
 
+// Напишите программу на C++ для реализации класса Circle с приватными переменными-членами для хранения радиуса.
+// Включите функции-члены для вычисления площади и длины окружности.
+
+class Circle {
+	private:
+		double radius;
+		double PI = 3.14;
+
+	public:
+		Circle(double r) : radius(r) {}
+
+		double area() const {
+			return PI * radius * radius;
+		}
+
+		double circumference() const {
+			return 2 * PI * radius;
+		}
+};
 
 int main()
 {
-	
-
+	Circle c(5);
+	std::cout << c.area() << std::endl;
+	std::cout << c.circumference() << std::endl;
 }
