c4b006c9d967af8a909c46b95df155578fa530f1
isHardCoded <ishardcoded@gmail.com>
2025-09-14 17:28:29 +0500

    Задание с библиотекой

--- ConsoleApplication47.cpp
+++ ConsoleApplication47.cpp
@@ -1,53 +1,201 @@
 ﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
-class Device {
+class Book {
 	protected:
-		std::string brand;
+		std::string title;
+		std::string author;
+		std::string ISBN;
+		bool available;
 
 	public:
-		Device(const std::string& brand) {
-			this->brand = brand;
+		Book(std::string t, std::string a, std::string i) 
+			: title(t), author(a), ISBN(i), available(true) {}
+
+		bool checkAvailability() const {
+			return available;
 		}
 
-		virtual void show() {
-			std::cout << "Device brand: " << brand << std::endl;
+		void setAvailability(bool avail) {
+			available = avail;
+		}
+
+		virtual std::string str() const {
+			return "Book: " + title + ", author: " + author + ", ISBN: " + ISBN +
+				", available: " + (available ? "Yes" : "No");
+		}
+
+		std::string getISBN() const {
+			return ISBN;
 		}
 };
 
-class Computer : public Device {
-	protected:
-		int ram;
+class EBook : public Book {
+private:
+	std::string fileFormat;
+
+public:
+	EBook(std::string t, std::string a, std::string i, std::string format) 
+		: Book(t, a, i), fileFormat(format) {}
+
+	std::string getFileFormat() const {
+		return fileFormat;
+	}
+
+	std::string str() const override {
+		return "Book: " + title + ", author: " + author + ", ISBN: " 
+			+ ISBN + ", Format: " + fileFormat +
+			", available: " + (available ? "Yes" : "No");
+	}
+};
+
+class PrintedBook : public Book {
+private:
+	int pageCount;
+
+public:
+	PrintedBook(std::string t, std::string a, std::string i, int pages)
+		: Book(t, a, i), pageCount(pages) {}
+
+	int getPageCount() const {
+		return pageCount;
+	}
+
+	std::string str() const override {
+		return "Book: " + title + ", author: " + author + ", ISBN: "
+			+ ISBN + ", Page count: " + std::to_string(pageCount) +
+			", available: " + (available ? "Yes" : "No");
+	}
+};
+
+class Reader {
+	private:
+		std::string name;
+		int reader_id;
+		std::vector<Book*> borrowedBooks;
 
 	public:
-		Computer(const std::string& b, int r) : Device(b), ram(r) {}
+		Reader(std::string n, int id) 
+			: name(n), reader_id(id) {}
 
-		void show() override {
-			std::cout << "Computer brand: " << brand << std::endl;
-			std::cout << "RAM: " << ram << std::endl;
+		void borrowBook(Book* book) {
+			if (book->checkAvailability()) {
+				borrowedBooks.push_back(book);
+				book->setAvailability(false);
+
+				std::cout << name << " borrowed" << book->str() << std::endl;
+			}
+			else {
+				std::cout << "Book is not available" << std::endl;
+			}
+		}
+
+		void returnBook(Book* book) {
+			auto it = find(borrowedBooks.begin(), borrowedBooks.end(), book);
+
+			if (it != borrowedBooks.end()) {
+				(*it)->setAvailability(true);
+				borrowedBooks.erase(it);
+				
+				std::cout << name << " returned" << std::endl;
+			}
+			else {
+				std::cout << name << " not borrowed by " << name << std::endl;
+			}
+		}
+
+		std::string str() const {
+			std::string res = "Reader: " + name + "\nBorrowed books:";
+			if (borrowedBooks.empty()) res += " None";
+
+			for (auto book : borrowedBooks) {
+				res += "\n - " + book->str();
+			}
+
+			return res;
 		}
 };
 
-class Phone : public Device {
-	protected:
-		std::string phone_number;
+class Librarian {
+	private:
+		std::string name;
+		int employee_id;
 
 	public:
-		Phone(const std::string& phone, const std::string& b) 
-			: Device(b), phone_number(phone) {}
+		Librarian(std::string n, int id) : name(n), employee_id(id) {}
 
-		void show() override {
-			std::cout << "Phone brand: " << brand << std::endl;
-			std::cout << "Phone number: " << phone_number << std::endl;
+		void addBook(std::vector<Book*>& books, Book* book) {
+			books.push_back(book);
+			std::cout << "Librarian " << name << " added book " << book->str() << std::endl;
+		}
+
+		void removeBook(std::vector<Book*>& books, Book* book) {
+			auto it = find(books.begin(), books.end(), book);
+			if (it != books.end()) {
+				std::cout << "Librarian " << name << " removed book: " << book->str() << std::endl;
+				books.erase(it);
+			}
+			else {
+				std::cout << "Book not found int library" << std::endl;
+			}
+		}
+
+		std::string str() const {
+			return "Librarian " + name;
 		}
 };
 
-class Smartphone : public Computer, public Phone {
+class Library {
+	private:
+		std::vector<Book*> books;
+		std::vector<Reader> readers;
+		std::vector<Librarian> librarians;
+
 	public:
-		Smartphone(const std::string& b, int r, const std::string& phone)
-			: Computer(b, r), Phone(phone, b) {}
+		void registerReader(const Reader& reader) {
+			readers.push_back(reader);
+			std::cout << "Registered reader: " << reader.str() << std::endl;
+		}
+
+		void addLibrarian(const Librarian& librarian) {
+			librarians.push_back(librarian);
+			std::cout << "Added librarian: " << librarian.str() << std::endl;
+		}
+
+		std::vector<Book*>& getBooks() {
+			return books;
+		}
+
+		std::vector<Reader> getReaders() const {
+			return readers;
+		}
+
+		std::vector<Librarian> getLibrarian() const {
+			return librarians;
+		}
 };
 
 int main()
 {
-	Computer computer("Samsung", 16);
+	Library lib;
+	Librarian lib1("Josh", 1);
+
+	lib.addLibrarian(lib1);
+
+	Book* b1 = new EBook("Test1", "test1", "test1", "pdf");
+	Book* b2 = new PrintedBook("Test2", "test2", "test3", 500);
+
+	lib1.addBook(lib.getBooks(), b1);
+	lib1.addBook(lib.getBooks(), b2);
+
+	Reader r1("John", 1);
+	lib.registerReader(r1);
+
+	r1.borrowBook(b1);
+	std::cout << r1.str() << std::endl;
+
+	r1.returnBook(b1);
+	std::cout << r1.str() << std::endl;
 }
