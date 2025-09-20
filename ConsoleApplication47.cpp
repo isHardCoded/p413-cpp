48643c6c996e88cc8e6bbbad4cc0f8664cff0764
isHardCoded <ishardcoded@gmail.com>
2025-09-20 16:30:25 +0500

    Static + destruct

--- ConsoleApplication47.cpp
+++ ConsoleApplication47.cpp
@@ -1,20 +1,67 @@
 ﻿#include <iostream>
 
-class Counter {
+class Employee {
+	private:
+		std::string name;
+		int id;
+		static int nextId;
+		static int employeeCount;
+
 	public:
-		static int count;
+		Employee(const std::string& employeeName) : name(employeeName) {
+			id = nextId++;
+			employeeCount++;
+		}
 
-		Counter() { ++count; }
+		~Employee() {
+			employeeCount--;
+		}
 
-		static int getCount() {
-			return count;
+		int getId() {
+			return id;
+		}
+
+		std::string getName() {
+			return name;
+		}
+
+		static int getEmployeeCount() {
+			return employeeCount;
+		}
+
+		static void reset() {
+			nextId = 1;
+			employeeCount = 0;
 		}
 };
 
-int Counter::count = 0;
+int Employee::nextId = 1;
+int Employee::employeeCount = 0;
 
 int main()
 {
-	Counter a, b, c;
-	std::cout << a.getCount();
+	std::cout << "Starting Employee count: " << Employee::getEmployeeCount() << std::endl;
+
+	Employee e1("John");
+	Employee e2("Tom");
+	Employee e3("Sam");
+
+	std::cout << "Employees created: " << std::endl;
+
+	std::cout << e1.getName() << " with ID " << e1.getId() << std::endl;
+	std::cout << e2.getName() << " with ID " << e2.getId() << std::endl;
+	std::cout << e3.getName() << " with ID " << e3.getId() << std::endl;
+
+	std::cout << "Current Employee count: " << Employee::getEmployeeCount() << std::endl;
+
+	{
+		Employee e4("Tim");
+		std::cout << "New employee created: " << e4.getName() << std::endl;
+		std::cout << "Employee count now: " << Employee::getEmployeeCount() << std::endl;
+	}
+
+	std::cout << "After destruct, employee count: " << Employee::getEmployeeCount() << std::endl;
+	
+	Employee::reset();
+	std::cout << "After rest, employee count: " << Employee::getEmployeeCount() << std::endl;
 }
