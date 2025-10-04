d5b3c94a1ad50be09461a466537e7ea4b0f87ed1
isHardCoded <ishardcoded@gmail.com>
2025-10-04 15:21:55 +0500

    get res

--- ConsoleApplication47.cpp
+++ ConsoleApplication47.cpp
@@ -1,32 +1,12 @@
 ﻿#include <iostream>
+#include <cpr/cpr.h>
 
-class Product {
-	private:
-		std::string name;
-		double priceUSD;
-		static double exchangeRate;
-	public:
-		Product(const std::string& n, double price) : name(n), priceUSD(price) {}
-
-		double getPrice() {
-			return priceUSD * exchangeRate;
-		}
-
-		static void setExchangeRate(double rate) {
-			exchangeRate = rate;
-		}
-};
-
-double Product::exchangeRate = 75.3;
+using namespace std;
+using namespace cpr;
 
 int main()
 {
-	Product p1("Phone", 800);
-	Product p2("PC", 1100);
+	auto response = cpr::Get(cpr::Url{"https://jsonplaceholder.typicode.com/users"});
 
-	Product::setExchangeRate(82.2);
-
-	std::cout << p1.getPrice() << std::endl;
-	std::cout << p2.getPrice() << std::endl;
-
+	std::cout << response.text;
 }
