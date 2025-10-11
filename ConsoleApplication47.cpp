4d513fcea42eadc4cab6570cc06339b31780469d
isHardCoded <ishardcoded@gmail.com>
2025-10-11 15:31:36 +0500

    added

--- ConsoleApplication47.cpp
+++ ConsoleApplication47.cpp
@@ -2,11 +2,38 @@
 #include <cpr/cpr.h>
 
 using namespace std;
-using namespace cpr;
+
+class HttpClient {
+	public:
+		virtual cpr::Response get(const string& url) = 0;
+		virtual cpr::Response post(const string& url, const string& body, const cpr::Header& headers) = 0;
+};
+
+class UserClient : public HttpClient {
+	private:
+		string BASE_URL;
+
+	public:
+		UserClient(const string& url) : BASE_URL(url) {}
+
+		cpr::Response get(const string& url) override {
+			return cpr::Get(cpr::Url{ url });
+		}
+
+		cpr::Response post(const string& url, const string& body, const cpr::Header& headers) override {
+			return cpr::Post(cpr::Url{ url }, cpr::Body{ body }, headers);
+		}
+
+		cpr::Response getUsers() {
+			return get(BASE_URL + "/users");
+		}
+};
 
 int main()
 {
-	auto response = cpr::Get(cpr::Url{"https://jsonplaceholder.typicode.com/users"});
+	UserClient client("https://6764432352b2a7619f5bfef7.mockapi.io");
 
-	std::cout << response.text;
+	cpr::Response response = client.getUsers();
+	std::cout << "GET status: " << response.status_code << std::endl;
+	std::cout << "GET body: " << response.text << std::endl;
 }
