86b5783b01f424063b3759776954a0dfc985929f
isHardCoded <ishardcoded@gmail.com>
2025-10-18 18:01:17 +0500

    currency

--- main.cpp
+++ main.cpp
@@ -1,29 +1,31 @@
 ﻿#include <iostream>
 #include <cpr/cpr.h>
 #include <nlohmann/json.hpp>
+#include <typeinfo>
 
 using json = nlohmann::json;
 
 int main()
 {
-	std::string apiKey = "c78f11918a7b4ce3b95192241252808";
-	std::string city;
-	std::cout << "Enter city: ";
-	std::cin >> city;
-	std::string url = "http://api.weatherapi.com/v1/current.json?key=" + apiKey + "&q=" + city;
+	std::string firstCurrency;
+	std::string secondCurrency;
+
+	std::cout << "Enter first currency: ";
+	std::cin >> firstCurrency;
+
+	std::cout << "Enter second currency: ";
+	std::cin >> secondCurrency;
+
+	std::string apiKey = "8a991d8624046f4421474b3c55539013";
+	std::string url = "https://currate.ru/api/?get=rates&pairs=" + firstCurrency + secondCurrency + "&key=" + apiKey;
 
 	cpr::Response response = cpr::Get(cpr::Url{ url });
 
 	if (response.status_code == 200) {
 		json j = json::parse(response.text);
-		std::cout << "Weather in " << city << std::endl;
-		std::cout << "Temp: " << j["current"]["temp_c"] << "°C" << std::endl;
-		std::cout << "Condition: " << j["current"]["condition"]["text"] << std::endl;
-		std::cout << "Humidity: " << j["current"]["humidity"] << " %" << std::endl;
-		std::cout << "Wind speed: " << j["current"]["wind_kph"] << " km/h" << std::endl;
+		std::cout << j["data"].value(firstCurrency + secondCurrency, "0") << std::endl;
 	}
 	else {
-		std::cout << response.status_code;
-		std::cout << response.text;
+		std::cout << "Error: " << response.status_code;
 	}
 }
