b5340b76a4a35b4167855ebccb8c4fa553b65c9c
isHardCoded <ishardcoded@gmail.com>
2025-11-09 16:12:13 +0500

    divide by zero

--- ConsoleApplication47.cpp
+++ ConsoleApplication47.cpp
@@ -1,8 +1,23 @@
 ﻿#include <iostream>
 
-
 int main()
 {
-	
+	try {
+		float a, b;
 
+		std::cout << "Put digit a: ";
+		std::cin >> a;
+
+		std::cout << "Put digit b: ";
+		std::cin >> b;
+
+		if (b == 0) {
+			throw b;
+		}
+
+		std::cout << "Result = " << (a / b) << std::endl;
+	}
+	catch (float error) {
+		std::cout << "Error - Divide by " << error << std::endl;
+	}
 }
