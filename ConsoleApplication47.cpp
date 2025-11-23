a4e5777759a38aa46b34dfa8ed36bc52d76d09c2
isHardCoded <ishardcoded@gmail.com>
2025-11-23 15:34:20 +0500

    task 1 completed

--- ConsoleApplication47.cpp
+++ ConsoleApplication47.cpp
@@ -1,8 +1,39 @@
 ﻿#include <iostream>
 
+class Student {
+	protected:
+		std::string name;
+		int age;
+		std::string group;
+
+	public:
+		Student(const std::string& name, int age, const std::string& group)
+			: name(name), age(age), group(group) {}
+
+		virtual void Print() const {
+			std::cout << "Student: " << name << std::endl;
+			std::cout << "Age: " << age << std::endl;
+			std::cout << "Group: " << group << std::endl;
+		}
+};
+
+class Aspirant : public Student {
+	private:
+		std::string workId;
+
+	public:
+		Aspirant(const std::string& name, int age, const std::string& group, const std::string& workId)
+			: Student(name, age, group), workId(workId) {}
+
+		void Print() const override {
+			std::cout << "Aspirant: " << name << std::endl;
+			std::cout << "Age: " << age << std::endl;
+			std::cout << "Group: " << group << std::endl;
+			std::cout << "Work ID: " << workId << std::endl;
+		}
+};
 
 int main()
 {
 	
-
 }
